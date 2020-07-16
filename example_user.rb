d885df242b768e4f9057fefefaaaeabdb61fd52a
haise578 <gffeva+@gmail.com>
2020-07-16 17:00:15 +0900

    :+1:class tutorial

---
+++ example_user.rb
@@ -0,0 +1,11 @@
+class User
+  attr_accessor :first_name, :last_name, :email
+  def initialize(attributes = {})
+    @first_name = attributes[:first_name]
+    @last_name = attributes[:last_name]
+    @email = attributes[:email]
+  end
+  def formatted_email
+    "#{@first_name} #{@last_name} <#{@email}>"
+  end
+end
